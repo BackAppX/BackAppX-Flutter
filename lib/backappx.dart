ea112e620c91ca2ee57364c139bf94d89364cc84
KvRae <karamelmannai@gmail.com>
2023-04-04 13:25:02 +0100

    working with auth methodes

--- lib/backappx.dart
+++ lib/backappx.dart
@@ -3,119 +3,56 @@ import 'package:http/http.dart' as http;
 
 
 class BackAppX {
-  // static const projectID = '5f9f9f9f9f9f9f9f9f9f9f9f';
+  static const projectRef = '';
+
+  // here you can add more static classes
   static Auth auth = Auth();
 }
 
 class Auth {
+  // here you can add routes for your auth class
   static const String _baseUrl = 'http://localhost:9092/user';
   static const _loginUrl = '$_baseUrl/login';
+  static const _registerUrl = '$_baseUrl/register';
+  static const _editUserUrl = '$_baseUrl/editUser';
+  static const _getUserUrl = '$_baseUrl/getUser';
 
-  static Future<bool> login(email, password) async {
+  static Future<http.Response> login(email, password) async {
     final response = await http.post(Uri.parse(_loginUrl), body: {
       'email': email,
       'password': password,
     });
 
-    if (response.statusCode == 200) {
-      print('login success');
-      print("----------------------------------");
-      print("${response.body}");
-      print("----------------------------------");
+    return response;
 
-      return true;
-    } else {
-      print('login failed');
-      print("----------------------------------");
-      print("${response.body}");
-      print("----------------------------------");
+  }
 
-      return false;
-    }
+  static Future<http.Response> register(name, email, password) async {
+    final response = await http.post(Uri.parse(_registerUrl), body: {
+      'name': name,
+      'email': email,
+      'password': password,
+      'project': BackAppX.projectRef,
+    });
+
+    return response;
+  }
+
+  static Future<http.Response> editUser(name, email, password) async {
+    final response = await http.put(Uri.parse(_editUserUrl), body: {
+      'name': name,
+      'email': email,
+      'password': password,
+    });
+
+    return response;
+  }
+
+  static Future<http.Response> getUser(id) async {
+    final response = await http.get(Uri.parse('$_getUserUrl/$id'));
+
+    return response;
   }
 }
 
-// static const String _registerUrl = '$_baseUrl/register';
-
-// static User user = User();
-// static Product product = Product();
-// static Order order = Order();
-
-
-//   static void register(name, email, password) {
-//     print('register');
-//     //post request to server
-//   }
-//
-//
-// }
-//
-// class User {
-//   static const String _baseUrl = 'http://localhost:8000/api';
-//   static const String _userUrl = '$_baseUrl/user';
-//   static void getUser() {
-//     print('get user');
-//     //get request to server
-//   }
-//
-//   static void updateUser() {
-//     print('update user');
-//     //put request to server
-//   }
-//
-//   static void deleteUser() {
-//     print('delete user');
-//     //delete request to server
-//   }
-// }
-//
-// class Product {
-//   static const String _baseUrl = 'http://localhost:8000/api';
-//   static const String _productUrl = '$_baseUrl/product';
-//   static void getProduct() {
-//     print('get product');
-//     //get request to server
-//   }
-//
-//   static void updateProduct() {
-//     print('update product');
-//     //put request to server
-//   }
-//
-//   static void deleteProduct() {
-//     print('delete product');
-//     //delete request to server
-//   }
-// }
-//
-// class Order {
-//   static const String _baseUrl = 'http://localhost:8000/api';
-//   static const String _orderUrl = '$_baseUrl/order';
-//   static void getOrder() {
-//     print('get order');
-//     //get request to server
-//   }
-//
-//   static void updateOrder() {
-//     print('update order');
-//     //put request to server
-//   }
-//
-//   static void deleteOrder() {
-//     print('delete order');
-//     //delete request to server
-//   }
-//   static void createOrder() {
-//     print('create order');
-//     //post request to server
-//   }
-//
-//   static void getOrders() {
-//     print('get orders');
-//     //get request to server
-//   }
-//   static void payOrder() {
-//     print('pay order');
-//     //post request to server
-//   }
 
