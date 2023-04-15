1044066ec4dda1f1e8656683fb1c046783478f8b
Zaghouani Yosri <yosri.zaghouani@esprit.tn>
2023-04-15 13:06:09 +0100

    Order + Product SDK

--- lib/backappx.dart
+++ lib/backappx.dart
@@ -1,12 +1,13 @@
 library backappx;
-import 'package:http/http.dart' as http;
 
+import 'package:http/http.dart' as http;
 
 class BackAppX {
   static const projectRef = '';
 
   // here you can add more static classes
   static Auth auth = Auth();
+  static Product product = Product();
   static Order order = Order();
 }
 
@@ -21,7 +22,8 @@ class Auth {
   static final _forgetPasswordUrl = '$_baseUrl/clientforgot-password';
   static final _resetPasswordUrl = '$_baseUrl/clientresetpassword';
 
-  static Future<http.Response> register(name,familyName, email, phoneNumber, password) async {
+  static Future<http.Response> register(
+      name, familyName, email, phoneNumber, password) async {
     final response = await http.post(Uri.parse(_registerUrl), body: {
       'name': name,
       'familyName': familyName,
@@ -41,10 +43,10 @@ class Auth {
     });
     print(response.body);
     return response;
-
   }
 
-  static Future<http.Response> editProfile(name, familyName,fullName, email,phoneNumber,password,image) async {
+  static Future<http.Response> editProfile(
+      name, familyName, fullName, email, phoneNumber, password, image) async {
     final response = await http.put(Uri.parse(_editClientUrl), body: {
       'name': name,
       'familyName': familyName,
@@ -75,7 +77,7 @@ class Auth {
     return response;
   }
 
-  static Future<http.Response> resetPassword(password,token) async {
+  static Future<http.Response> resetPassword(password, token) async {
     final response = await http.post(Uri.parse(_resetPasswordUrl), body: {
       'newPass': password,
       'resetLink': token,
@@ -85,7 +87,102 @@ class Auth {
   }
 }
 
+class Order {
+  // here you can add routes for your auth class
+  static final String _baseUrl = 'http://localhost:9092/order';
 
-class Order {}
+  static final _GetOrder = '$_baseUrl/orders';
+  static final _AddOrder = '$_baseUrl/addorder';
+  static final _GetOrderByID = '$_baseUrl/:orderId';
+  static final _DeleteOrder = '$_baseUrl/:orderId';
 
+  static Future<http.Response> getOrder(id) async {
+    final response = await http.get(Uri.parse(_GetOrder));
 
+    print(response.body.toString());
+    return response;
+  }
+
+  static Future<http.Response> addOrder(quantity, productId, clientId) async {
+    final response = await http.post(Uri.parse(_AddOrder), body: {
+      'quantity': quantity,
+      'productId': productId,
+      'clientId': clientId,
+      //'reference': BackAppX.projectRef,
+      //'image': image
+    });
+    print(response.body);
+    return response;
+  }
+
+  static Future<http.Response> getSingleOrder(id) async {
+    final response = await http.get(Uri.parse('$_GetOrderByID / $id'));
+
+    print(response.body.toString());
+    return response;
+  }
+
+  static Future<http.Response> deleteOrder(id) async {
+    final response = await http.delete(Uri.parse(_DeleteOrder));
+
+    print(response.body.toString());
+    return response;
+  }
+}
+
+class Product {
+  // here you can add routes for your auth class
+  static final String _baseUrl = 'http://localhost:9092/product';
+
+  static final _Getproduct = '$_baseUrl/products';
+  static final _getSingleProduct = '$_baseUrl/:productId';
+  static final _UpdateProduct = '$_baseUrl/:productId';
+  static final _DeleteProduct = '$_baseUrl/:productId';
+  static final _addProduct = '$_baseUrl/Addproduct';
+
+  static Future<http.Response> getProduct(id) async {
+    final response = await http.get(Uri.parse(_Getproduct));
+
+    print(response.body.toString());
+    return response;
+  }
+
+  static Future<http.Response> getSingleProduct(id) async {
+    final response = await http.get(Uri.parse('$_getSingleProduct / $id'));
+
+    print(response.body.toString());
+    return response;
+  }
+
+  static Future<http.Response> editProduct(name, price, quantity) async {
+    final response = await http.put(Uri.parse(_UpdateProduct), body: {
+      'name': name,
+      'price': price,
+      'quantity': quantity,
+      //'reference': BackAppX.projectRef,
+      //'image': image
+    });
+
+    print(response.body);
+    return response;
+  }
+
+  static Future<http.Response> deleteProduct(id) async {
+    final response = await http.delete(Uri.parse(_DeleteProduct));
+
+    print(response.body.toString());
+    return response;
+  }
+
+  static Future<http.Response> addProduct(quantity, product, client) async {
+    final response = await http.post(Uri.parse(_addProduct), body: {
+      'quantity': quantity,
+      'product': product,
+      'client': client,
+      //'reference': BackAppX.projectRef,
+      //'image': image
+    });
+    print(response.body);
+    return response;
+  }
+}
