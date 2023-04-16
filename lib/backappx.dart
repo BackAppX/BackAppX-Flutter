9a119de78a37c9cdf5555ad526a1d5bbcc08e5c2
Zaghouani Yosri <yosri.zaghouani@esprit.tn>
2023-04-16 21:56:56 +0100

    category SDK

--- lib/backappx.dart
+++ lib/backappx.dart
@@ -9,6 +9,7 @@ class BackAppX {
   static Auth auth = Auth();
   static Product product = Product();
   static Order order = Order();
+  static Categorie categorie = Categorie();
 }
 
 class Auth {
@@ -184,5 +185,45 @@ class Product {
     });
     print(response.body);
     return response;
+  }
+}
+
+class Categorie {
+  static final String _baseUrl = 'http://localhost:9092/categorie';
+
+  static final _DeleteCategorie = '$_baseUrl/:CategorieID';
+  static final _addCategorie = '$_baseUrl/Addproduct';
+
+  static Future<http.Response> DeleteCategorie(id) async {
+    final response = await http.delete(Uri.parse(_DeleteCategorie));
+
+    print(response.body.toString());
+    return response;
+  }
+
+  static Future<http.Response> addCategorie(name) async {
+    final response = await http.post(Uri.parse(_addCategorie), body: {
+      'name': name,
+    });
+    print(response.body);
+    return response;
+  }
+
+  Future<http.Response> addCategoryToProduct(
+      String productId, String categoryId) async {
+    final String baseUrl = 'http://localhost:9092';
+    final String endpoint = '/product/$productId/category';
+    final url = Uri.parse('$baseUrl$endpoint');
+
+    try {
+      final response = await http.post(
+        url,
+        body: {'categoryId': categoryId},
+      );
+      return response;
+    } catch (error) {
+      print(error.toString());
+      return Future.value(http.Response('Error', 500));
+    }
   }
 }
