34af5519513e904f09a7e3593df27f6de432257a
KvRae <karamelmannai@gmail.com>
2023-05-09 18:29:33 +0100

    manyto one classes

--- lib/backappx.dart
+++ lib/backappx.dart
@@ -6,21 +6,34 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:path/path.dart' as path;
 
-class Auth {
-  static final _baseUrl = 'http://localhost:9092/client';
-  static final _registerUrl = '$_baseUrl/client';
-  static final _loginUrl = '$_baseUrl/login';
-  static final _editClientUrl = '$_baseUrl/client';
-  static final _getClientUrl = '$_baseUrl/clientbyid';
-  static final _forgetPasswordUrl = '$_baseUrl/clientforgot-password';
+class BackAppX {
+  static final BackAppX _instance = BackAppX._internal(); // singleton
 
-  static final Auth _instance = Auth._internal();
-
-  factory Auth() {
-    return _instance;
+  factory BackAppX() {
+    return _instance; // return the same object
   }
 
-  Auth._internal();
+  BackAppX._internal(); // private constructor
+
+  /**********************************************************************************************************/
+  /************************************* Project Management ***********************************************/
+  /// ********************************************************************************************************/
+
+  static const String projectRef =
+      ''; // this is the project reference that you get from the dashboard
+  static const String stripeKey =
+      ''; //this is the stripe key that you get from the dashboard
+  static const String _baseUrl = 'http://localhost:9092';
+
+  /********************************************************************************************************/
+  //*************************************** Client Auth ***************************************************/
+  /// ******************************************************************************************************/
+
+  static const _registerUrl = '$_baseUrl/client/client';
+  static const _loginUrl = '$_baseUrl/client/login';
+  static const _editClientUrl = '$_baseUrl/client/client';
+  static const _getClientUrl = '$_baseUrl/client/clientbyid';
+  static const _forgetPasswordUrl = '$_baseUrl/client/clientforgot-password';
 
   Future<http.Response> register(
       name, familyName, email, phoneNumber, password) async {
@@ -30,9 +43,9 @@ class Auth {
       'email': email,
       'phoneNumber': phoneNumber,
       'password': password,
-      'reference': Project.projectRef
+      'reference': BackAppX.projectRef
     });
-    if (Project.projectRef == null) {
+    if (projectRef == null) {
       return http.Response('Please set the project reference', 400);
     }
 
@@ -57,10 +70,10 @@ class Auth {
       'email': email,
       'phoneNumber': phoneNumber,
       'password': password,
-      'reference': Project.projectRef,
+      'reference': projectRef,
       'image': image
     });
-    if (Project.projectRef == null) {
+    if (projectRef == null) {
       return http.Response('Please set the project reference', 400);
     }
 
@@ -78,25 +91,16 @@ class Auth {
     });
     return response;
   }
-}
 
-class Category {
-  static final Category _instance = Category._internal();
+  /********************************************************************************************************/
+  /************************************* Category Management ***********************************************/
+  /// ******************************************************************************************************/
 
-  factory Category() {
-    return _instance;
-  }
+  static const _deleteCategorie = '$_baseUrl/categorie/:CategorieID';
+  static const _addCategorie = '$_baseUrl/categorie/Addproduct';
 
-  Category._internal();
-
-  static final String _baseUrl = 'http://localhost:9092/categorie';
-  static final _DeleteCategorie = '$_baseUrl/:CategorieID';
-  static final _addCategorie = '$_baseUrl/Addproduct';
-
-  static Future<http.Response> DeleteCategorie(id) async {
-    final response = await http.delete(Uri.parse(_DeleteCategorie));
-
-    print(response.body.toString());
+  static Future<http.Response> deleteCategorie(id) async {
+    final response = await http.delete(Uri.parse(_deleteCategorie));
     return response;
   }
 
@@ -104,13 +108,12 @@ class Category {
     final response = await http.post(Uri.parse(_addCategorie), body: {
       'name': name,
     });
-    print(response.body);
     return response;
   }
 
   Future<http.Response> addCategoryToProduct(
       String productId, String categoryId) async {
-    final String baseUrl = 'http://localhost:9092';
+    const String baseUrl = 'http://localhost:9092';
     final String endpoint = '/product/$productId/category';
     final url = Uri.parse('$baseUrl$endpoint');
 
@@ -121,33 +124,25 @@ class Category {
       );
       return response;
     } catch (error) {
-      print(error.toString());
       return Future.value(http.Response('Error', 500));
     }
   }
-}
 
-class FileStorage {
-static final FileStorage _instance = FileStorage._internal();
+  /********************************************************************************************************/
+  /************************************* File Storage Management *******************************************/
+  /// ******************************************************************************************************/
 
-  factory FileStorage() {
-    return _instance;
-  }
-
-  FileStorage._internal();
-
-  static const String _baseUrl = 'http://localhost:9092/project';
-  static const String _addFileUrl = '$_baseUrl/upload';
-  static const String _getFilesUrl = '$_baseUrl/images';
-  static const String _getSingleFileUrl = '$_baseUrl/file';
-  static const String _deleteFileUrl = '$_baseUrl/image';
+  static const String _addFileUrl = '$_baseUrl/project/upload';
+  static const String _getFilesUrl = '$_baseUrl/project/images';
+  static const String _getSingleFileUrl = '$_baseUrl/project/file';
+  static const String _deleteFileUrl = '$_baseUrl/project/image';
 
   static Future<http.Response> addFile(File file, projectId) async {
-    final stream = new http.ByteStream(file.openRead());
+    final stream = http.ByteStream(file.openRead());
     final length = await file.length();
     final uri = Uri.parse('$_addFileUrl/$projectId');
-    final request = new http.MultipartRequest('PUT', uri);
-    final multipartFile = new http.MultipartFile('file', stream, length,
+    final request = http.MultipartRequest('PUT', uri);
+    final multipartFile = http.MultipartFile('file', stream, length,
         filename: path.basename(file.path));
     request.files.add(multipartFile);
     final response = await request.send();
@@ -166,7 +161,7 @@ static final FileStorage _instance = FileStorage._internal();
 
   static Future<http.Response> getFile(projectId, imageName) async {
     final response =
-    await http.get(Uri.parse("$_getSingleFileUrl/$projectId/$imageName"));
+        await http.get(Uri.parse("$_getSingleFileUrl/$projectId/$imageName"));
     if (response.statusCode == 200) {
       final responseBody = json.encode(response.body);
       return http.Response(responseBody, response.statusCode);
@@ -177,7 +172,7 @@ static final FileStorage _instance = FileStorage._internal();
 
   static Future<http.Response> deleteFile(projectId, imageName) async {
     final response =
-    await http.delete(Uri.parse('$_deleteFileUrl/$projectId/$imageName'));
+        await http.delete(Uri.parse('$_deleteFileUrl/$projectId/$imageName'));
     if (response.statusCode == 200) {
       final responseBody = json.encode(response.body);
       return http.Response(responseBody, response.statusCode);
@@ -185,129 +180,93 @@ static final FileStorage _instance = FileStorage._internal();
       throw Exception('Failed to get files:${response.statusCode}');
     }
   }
-}
 
-class Order {
-  static final String _baseUrl = 'http://localhost:9092/order';
-  static final _GetOrder = '$_baseUrl/orders';
-  static final _AddOrder = '$_baseUrl/addorder';
-  static final _GetOrderByID = '$_baseUrl/:orderId';
-  static final _DeleteOrder = '$_baseUrl/:orderId';
+  /********************************************************************************************************/
+  /************************************* Order Management ***********************************************/
+  /// ******************************************************************************************************/
 
-  static final Order _instance = Order._internal();
-
-  factory Order() {
-    return _instance;
-  }
-
-  Order._internal();
+  static const _getOrder = '$_baseUrl/order/orders';
+  static const _addOrder = '$_baseUrl/order/addorder';
+  static const _getOrderByID = '$_baseUrl/order/:orderId';
+  static const _deleteOrder = '$_baseUrl/order/:orderId';
 
   Future<http.Response> getOrder(id) async {
-    final response = await http.get(Uri.parse(_GetOrder));
-
-    print(response.body.toString());
+    final response = await http.get(Uri.parse(_getOrder));
     return response;
   }
 
   Future<http.Response> addOrder(quantity, productId, clientId) async {
-    final response = await http.post(Uri.parse(_AddOrder), body: {
+    final response = await http.post(Uri.parse(_addOrder), body: {
       'quantity': quantity,
       'productId': productId,
       'clientId': clientId,
-      'reference': Project.projectRef,
+      'reference': projectRef,
       //'image': image
     });
     return response;
   }
 
   Future<http.Response> getSingleOrder(id) async {
-    final response = await http.get(Uri.parse('$_GetOrderByID / $id'));
+    final response = await http.get(Uri.parse('$_getOrderByID / $id'));
     return response;
   }
 
   Future<http.Response> deleteOrder(id) async {
-    final response = await http.delete(Uri.parse(_DeleteOrder));
+    final response = await http.delete(Uri.parse(_deleteOrder));
     return response;
   }
-}
 
-class Payment {
-  static final _baseUrl = 'http://localhost:9092/paymentService';
-  static final _pay = '$_baseUrl/pay';
-  static final Payment _instance = Payment._internal();
+  /********************************************************************************************************/
+  /************************************* Payment Management ***********************************************/
+  /// ******************************************************************************************************/
 
-  factory Payment() {
-    return _instance;
-  }
-
-  Payment._internal();
+  static const _pay = '$_baseUrl/paymentService/pay';
 
   Future<http.Response> pay(orderId, cardElementId) async {
     final response = await http.post(Uri.parse(_pay), headers: {
       'stripekey':
-      Project.stripeKey, //this is the stripe key that you get from the dashboard
+          stripeKey, //this is the stripe key that you get from the dashboard
     }, body: {
       'orderId':
-      orderId, //this orderId is the id of the order returned by the order
+          orderId, //this orderId is the id of the order returned by the order
       "payment_method":
-      cardElementId, //this cardElementId is the id of the card element returned by stripe from cardElement.create()
+          cardElementId, //this cardElementId is the id of the card element returned by stripe from cardElement.create()
       "confirmation_method":
-      "manual", //this is the confirmation method of the payment
-      'reference': Project.projectRef, //this is the reference of the project
+          "manual", //this is the confirmation method of the payment
+      'reference': projectRef, //this is the reference of the project
     });
-    if (Project.stripeKey == null) {
+    if (stripeKey == null) {
       return http.Response('Please set the stripe key', 400);
     }
     return response;
   }
-}
 
-class Project {
-  static final String
-  projectRef= ''; // this is the project reference that you get from the dashboard
-  static final String
-  stripeKey= '' ; //this is the stripe key that you get from the dashboard
-  static final Project _instance = Project._internal();
-  factory Project() {
-    return _instance;
-  }
-  Project._internal();
-}
+/********************************************************************************************************/
+/************************************* Product Management ***********************************************/
+  /// ******************************************************************************************************/
 
-class Product {
-  static final String _baseUrl = 'http://localhost:9092/product';
-  static final _Getproduct = '$_baseUrl/products';
-  static final _getSingleProduct = '$_baseUrl';
-  static final _UpdateProduct = '$_baseUrl/:productId';
-  static final _DeleteProduct = '$_baseUrl/:productId';
-  static final _addProduct = '$_baseUrl/Addproduct';
-
-  static final Product _instance = Product._internal();
-
-  factory Product() {
-    return _instance;
-  }
-
-  Product._internal();
+  static const _getproduct = '$_baseUrl/product/products';
+  static const _getSingleProduct = '$_baseUrl/product';
+  static const _updateProduct = '$_baseUrl/product/:productId';
+  static const _deleteProduct = '$_baseUrl/product/:productId';
+  static const _addProduct = '$_baseUrl/product/Addproduct';
 
   Future<http.Response> getProduct(id) async {
-    final response = await http.get(Uri.parse(_Getproduct));
+    final response = await http.get(Uri.parse(_getproduct));
     return response;
   }
 
   Future<http.Response> getSingleProduct(id) async {
     final response = await http.get(Uri.parse('$_getSingleProduct/$id'));
-
-    print(response.body.toString());
     return response;
   }
 
   Future<http.Response> editProduct(name, price, quantity) async {
-    final response = await http.put(Uri.parse(_UpdateProduct), body: {
+    final response = await http.put(Uri.parse(_updateProduct), body: {
       'name': name,
       'price': price,
       'quantity': quantity,
-      'reference': Project.projectRef,
+      'reference': projectRef,
       //'image': image
     });
 
@@ -315,7 +274,7 @@ class Product {
   }
 
   Future<http.Response> deleteProduct(id) async {
-    final response = await http.delete(Uri.parse(_DeleteProduct));
+    final response = await http.delete(Uri.parse(_deleteProduct));
 
     return response;
   }
@@ -325,7 +284,7 @@ class Product {
       'quantity': quantity,
       'product': product,
       'client': client,
-      'reference': Project.projectRef,
+      'reference': projectRef,
       //'image': image
     });
     return response;
